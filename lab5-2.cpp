ce1dc3ed16955076d5ecd98e07c892b3e7990898
olgastrozier <olgastrozier@my.tridenttech.edu>
2024-07-25 20:38:42 -0400

    Update lab5-2.cpp

--- lab5-2.cpp
+++ lab5-2.cpp
@@ -7,6 +7,33 @@ using namespace std;
 
 int main()
 {
+	const double DISCOUNT_RATE = 0.1;
+	const double SHIP_CHG1 = 0.99;
+	const double SHIP_CHG2 = 4.99;
+	double amtOwed = 0.0;
+	char member = ' ';
 
+	cout << "Amount owed before any discount and shipping: ";
+	cin >> amtOwed;
+	cout << "Premier Club member (Y/N)? ";
+	cin >> member;
+
+	//subtract discount, if appropriate
+	if (toupper(member) == 'Y')
+	amtOwed -= amtOwed * DISCOUNT_RATE;
+	//end if
+	
+	//add shipping
+	if (amtOwed >= 100.0)
+	amtOwed += SHIP_CHG1;
+	else
+	amtOwed += SHIP_CHG2;
+	//end if
+	
+	//display final amount owed
+	cout << fixed << setprecision(2);
+	cout << "Amount owed after any discount and shipping: "
+	<< amtOwed << endl;
+	
 	return 0;
 }	//end of main function

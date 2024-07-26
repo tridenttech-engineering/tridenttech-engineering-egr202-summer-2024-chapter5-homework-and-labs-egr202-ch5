db48b4b9d5ea08cacdbc9136972b9724d7916564
olgastrozier <olgastrozier@my.tridenttech.edu>
2024-07-25 20:44:31 -0400

    Update lab5-6.cpp

--- lab5-6.cpp
+++ lab5-6.cpp
@@ -22,7 +22,7 @@ int main()
 	//calculate service charge
 	serviceChg = purchase * 0.01;
 	//add $5 to code 2 purchases
-	if (code = '2')
+	if (code == '2')
 		serviceChg = serviceChg + 5;
 	//end if
 
